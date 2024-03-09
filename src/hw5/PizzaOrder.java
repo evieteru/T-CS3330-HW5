e5f9c79b193c91899b1b403b11d92b8e4a319722
23wc01 <132469274+23wc01@users.noreply.github.com>
2024-03-09 14:02:04 -0800

    Update PizzaOrder.java - Added fields & constructor for PizzaOrder

--- src/hw5/PizzaOrder.java
+++ src/hw5/PizzaOrder.java
@@ -1,5 +1,11 @@
 package hw5;
 
 public class PizzaOrder {
-
+	private PizzaCookingFactory pizzaFactory;
+	private ICookingStrategy cookingStrategy; // This is useless bc not all pizzas are cooked same way in a cart!
+	List<AbstractPizza> pizzaOrderList;
+	public PizzaOrder() {
+		pizzaFactory = new PizzaCookingFactory(); 
+		pizzaOrderList = new ArrayList<AbstractPizza>();
+	}
 }
