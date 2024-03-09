01dbbeb1f232fb463729ae82bc8eba20bbdaf6c2
23wc01 <132469274+23wc01@users.noreply.github.com>
2024-03-09 13:50:00 -0800

    Update PizzaCookingFactory.java - added the switch case for factory method implementation of different pizza type creation.  Modifies orderIDCounter to increment by 1 each time a pizza is ordered

--- src/Cooking/PizzaCookingFactory.java
+++ src/Cooking/PizzaCookingFactory.java
@@ -4,23 +4,30 @@ import hw5.PizzaType;
 import pizzas.AbstractPizza;
 
 public class PizzaCookingFactory {
-
 	public AbstractPizza createPizza(PizzaType pizzaType) {
 		
-		switch (pizzaType) {
-		case PizzaType.HAWAIIAN:
-			HawaiianPizza newHawaiian = new Hawaiian()
-			
-		case PizzaType.MARGHERITA:
-			
-		case PizzaType.SUPREME:
-			
-		case PizzaType.VEGETARIAN:
-			
-
-			
-			
+		switch(pizzaType) {
+		case MARGHERITA:
+			AbstractPizza new_margherita = new MargheritaPizza();
+			new_margherita.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+			return new_margherita;
+		case VEGETARIAN:
+			AbstractPizza new_vegetarian = new VegetarianPizza();
+			new_vegetarian.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+			return new_vegetarian;
+		case HAWAIIAN:
+			AbstractPizza new_hawaiian = new HawaiianPizza();
+			new_hawaiian.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+			return new_hawaiian;
+		case SUPREME:
+			AbstractPizza new_supreme = new SupremePizza();
+			new_supreme.setPizzaOrderID(AbstractPizza.getOrderIDCounter());
+			return new_supreme;
+		default:
+			System.out.println("Invalid pizzaType passed in");
+			return null;
 		}
-	}
 		
+	}
+
 }
