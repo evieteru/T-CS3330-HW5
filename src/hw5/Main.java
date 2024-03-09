6cbeb21c06f60c7c5a421b605758201fec275949
23wc01 <132469274+23wc01@users.noreply.github.com>
2024-03-09 14:31:17 -0800

    Update Main.java - Added some clarifying comments for testing

--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -6,14 +6,20 @@ public class Main {
 		// Instantiate a pizzaOrder, perform operations based on the requirements.
 		PizzaOrder order = new PizzaOrder();
 		order.addPizzaToCart(PizzaType.HAWAIIAN);
+		
+		// Should find & print out info for Hawaiian pizza (with no modifications)
 		HawaiianPizza p1 = (HawaiianPizza)order.getPizzaByOrderID(0);
 		System.out.println(p1);
-		order.addNewToppingToPizza(0, Toppings.MUSHROOM);
-		order.selectCookingStrategyByPizzaOrderID(0, CookingStyleType.BRICK_OVEN);
 		
+		// Testing the add toppings function on 1st pizza added (the p1 Hawaiian pizza should also now have mushrooms)
+		order.addNewToppingToPizza(0, Toppings.MUSHROOM);
+		
+		// Ensure that cookingPrice & totalPrice & cookingStrategy is updated accordingly
+		order.selectCookingStrategyByPizzaOrderID(0, CookingStyleType.BRICK_OVEN);
+		// Add new pizza
 		order.addPizzaToCart(PizzaType.SUPREME);
-		SupremePizza p2 = (SupremePizza)order.getPizzaByOrderID(1);
-		System.out.println(p2);
+		
+		// This should be false bc the mushroom should already be in a supreme pizza
 		System.out.println(order.addNewToppingToPizza(1, Toppings.MUSHROOM));
 		order.selectCookingStrategyByPizzaOrderID(1, CookingStyleType.MICROWAVE);
 		order.printPizzaOrderCart(1);
