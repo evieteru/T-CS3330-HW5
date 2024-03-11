2be05c0128d33623072db427476fbacefc0e94d6
Alyssa <AlyssaRSchaefer@gmail.com>
2024-03-11 17:16:10 -0500

    added MicrowaveOvenCookingStrategy

--- src/Cooking/MicrowaveOvenCookingStrategy.java
+++ src/Cooking/MicrowaveOvenCookingStrategy.java
@@ -1,5 +1,19 @@
 package Cooking;
 
-public class MicrowaveOvenCookingStrategy {
+import pizzas.AbstractPizza;
 
+public class MicrowaveOvenCookingStrategy {
+	
+		// Override interface cook method
+		public boolean cook(AbstractPizza pizza) {
+			
+			// Set cooking price
+			pizza.setCookingPrice(1);
+			// Add cooking price to total price
+			pizza.updatePizzaPrice();
+			// Set cooking strategy according to correct enum value
+			pizza.setCookingStrategy(CookingStyleType.BRICK_OVEN);
+			
+			return true;
+		}
 }
