88489981974435bf0be06a941b244eab6f979adf
Alyssa <AlyssaRSchaefer@gmail.com>
2024-03-11 17:12:31 -0500

    added ConventionalOvenCookingStrategy

--- src/Cooking/BrickOvenCookingStrategy.java
+++ src/Cooking/BrickOvenCookingStrategy.java
@@ -1,5 +1,21 @@
 package Cooking;
 
-public class BrickOvenCookingStrategy {
+import pizzas.AbstractPizza;
 
+public class BrickOvenCookingStrategy implements ICookingStrategy{
+
+	// Override interface cook method
+	public boolean cook(AbstractPizza pizza) {
+		
+		// Set cooking price
+		pizza.setCookingPrice(10);
+		// Add cooking price to total price
+		pizza.updatePizzaPrice();
+		// Set cooking strategy according to correct enum value
+		pizza.setCookingStrategy(CookingStyleType.BRICK_OVEN);
+		
+		return true;
+		}
+		
+	}
 }
--- src/Cooking/ConventionalOvenCookingStrategy.java
+++ src/Cooking/ConventionalOvenCookingStrategy.java
@@ -1,5 +1,18 @@
 package Cooking;
 
-public class ConventionalOvenCookingStrategy {
+import pizzas.AbstractPizza;
 
+public class ConventionalOvenCookingStrategy {
+	// Override interface cook method
+		public boolean cook(AbstractPizza pizza) {
+			
+			// Set cooking price
+			pizza.setCookingPrice(10);
+			// Add cooking price to total price
+			pizza.updatePizzaPrice();
+			// Set cooking strategy according to correct enum value
+			pizza.setCookingStrategy(CookingStyleType.CONVENTIONAL_OVEN);
+			
+			return true;
+			}
 }
